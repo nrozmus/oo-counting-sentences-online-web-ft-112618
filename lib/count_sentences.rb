10659c9b3e8a046b0ce3e9fab18b90f0721f6a8c
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-12-07 18:09:10 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") if true
   end
 
   def question?
-
+    self.end_with?("?") if true
   end
 
   def exclamation?
-
+    self.end_with?("!") if true
   end
 
-  def count_sentences
-
+  def count_sentences 
+    sentences = self.split(/[.?!]/)
+    sentences.each do |sentence|
+      if sentence == ""
+        sentences.delete(sentence)
+      end
+    end
+      #binding.pry
+     return sentences.count
   end
 end
